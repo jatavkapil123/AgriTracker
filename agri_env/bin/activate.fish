ab1a689a60e6ed9637ff1053f149e93c72fccc08
jatavkapil123 <kapiljatav4991@gmail.com>
2026-01-20 14:03:42 +0530

    changes

--- agri_env/bin/activate.fish
+++ agri_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kapil-jatav/Desktop/djngoApplication/agri_env
+set -gx VIRTUAL_ENV /home/kapil-jatav/AgriTracker/agri_env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
